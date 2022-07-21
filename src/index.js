1f0ddb7a0227d36cbbaea54dcce98d01447c388d
Bruno Cruz <brunocruz012013@gmail.com>
2022-07-20 23:27:16 -0300

    Criando rota de cadastro

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+// para renomear usa os : e o novo nome
+const { v4: uuidv4 } = require('uuid'); // o v4 vai gerar o id com números randomicos
 
 const app = express();
 
-app.get('/', (request, response) => {
-   return response.send("Olá!");
+app.use(express.json());
+
+const customers = []
+/**
+ * cpf - string
+ * name - string
+ * id - uuid (Universal Unique Identifiers, Identificador Universal)
+ * statement - []
+ */
+
+// criar uma conta
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4(); // vai gerar o id
+    // criar um banco de dados "fake"
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+    return response.status(201).send(); // 201 - quando dado é criado
 });
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
     console.log("Servidor rodando na posta 3333");
 })
