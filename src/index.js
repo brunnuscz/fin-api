2c46d55ccf55510afddd141e27f950e1d32adacf
Bruno Cruz <brunocruz012013@gmail.com>
2022-07-22 11:43:46 -0300

    Criando middleware, fazendo busca a partir do cpf

--- src/index.js
+++ src/index.js
@@ -16,6 +16,25 @@ const customers = []
  * statement - []
  */
 
+// middlewares é aquilo que ta no meio, é como se fosse uma função entre o request e o response
+function verifyExistsAccountCPF(request, response, next) { // o next define se vai pra frente ou vai parar onde tá
+    // desestruturando
+    const { cpf } = request.headers; // passando valor no header no insomnia
+    // o cliente e dentro do cliente buscar o statement
+    const customer = customers.find( // o find retorna todos os dados
+        customer => customer.cpf === cpf
+    );
+    // verificar se não tem nenhum cpf
+    // quando o objeto não tiver preenchido
+    if (!customer) { // ! significa que ta negando
+        return response.status(400).json({ error: "Customer not found!" });
+    }
+    // passando o obj que vai ser repassado
+    request.customer = customer;
+
+    return next();
+}
+
 // criar uma conta
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
@@ -25,8 +44,8 @@ app.post('/account', (request, response) => {
         (customers) => customers.cpf === cpf
     );
     // se for true isso quer dizer que cpf já existe
-    if(customersAlreadyExists){
-        return response.status(400).json({error: "Customer already exists!"});
+    if (customersAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
     }
 
     // criar um banco de dados "fake"
@@ -38,6 +57,16 @@ app.post('/account', (request, response) => {
     });
     return response.status(201).send(); // 201 - quando dado é criado
 });
+
+// buscar extrato bancario, cpf vai ser recebido pelo router params
+// o middleware pode ser passado como segundo parâmetro ou
+// app.use(verifyExistsAccountCPF); tem que ficar a cima dos que forem utilizar
+app.get('/statement', verifyExistsAccountCPF, (request, response) => {
+    const {customer} = request; // informação dentro do request
+    return response.json(customer.statement);
+});
+
+
 
 app.listen(3333, () => {
     console.log("Servidor rodando na posta 3333");
